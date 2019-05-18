16b644e6336ef41237c53592b936bd5a40bad466
Slava <zakharovvu@gmail.com>
2019-05-18 19:10:27 +0300

    Updates

---
+++ precache-manifest.56492bb69054a457a3ed1827e1d201b9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d784a736931ecd84e1d4715f01dfaea1",
+    "url": "/calendar_films/index.html"
+  },
+  {
+    "revision": "0e25f9377e21a51a0f7a",
+    "url": "/calendar_films/static/css/main.81b68568.chunk.css"
+  },
+  {
+    "revision": "4b28d1f284c0b419379c",
+    "url": "/calendar_films/static/js/2.5335202a.chunk.js"
+  },
+  {
+    "revision": "0e25f9377e21a51a0f7a",
+    "url": "/calendar_films/static/js/main.55382909.chunk.js"
+  },
+  {
+    "revision": "cd5d9e884f99a344a162",
+    "url": "/calendar_films/static/js/runtime~main.524a6d8b.js"
+  }
+]);
